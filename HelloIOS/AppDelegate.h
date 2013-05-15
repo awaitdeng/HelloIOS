0b33474dc4ab744f8aa72d89e70201aefafd22e7
luyd <awaitdeng@163.com>
2013-05-15 21:51:56 +0800

    Initial Commit

---
+++ HelloIOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloIOS
+//
+//  Created by apple on 13-5-15.
+//  Copyright (c) 2013年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
